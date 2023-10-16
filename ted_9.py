f8adf601b5fb28375239ed6302a6acff939e487b
Layse Bruna Cabral <143559985+Laysecabral@users.noreply.github.com>
2023-10-15 21:43:39 -0300

    Add files via upload
    Glossário = dicionário

---
+++ ted_9.py
@@ -0,0 +1,10 @@
+glossario = {
+    'Algoritmos': 'Conjunto de instruções para realizar uma tarefa específica.',
+    'Python': 'Uma linguagem de programação.',
+    'Webscraping': 'O processo de extrair informações de websites.',
+    'Lógica de Programação': 'A base fundamental para escrever programas de computador.',
+    'Google Colab': 'Um ambiente de desenvolvimento em nuvem gratuito.',
+    'Visual Studio Code': 'Um editor de código gratuito e de código aberto desenvolvido pela Microsoft.',
+}
+for palavra, significado in glossario.items():
+    print(f"{palavra}:\n{significado}\n")
