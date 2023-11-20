3c2b55aaae04f6d5457c88d1cd08c6b964e34a0b
Layse Bruna Cabral <143559985+Laysecabral@users.noreply.github.com>
2023-11-20 19:34:36 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,11 @@
+with open('Moby-Dick.txt', 'r', encoding='utf-8') as livro:
+    conteudo = livre.read()
+    linhas = livro.readlines()
+    qtd_linhas = len()
+
+    print(conteudo)
+    print('-----------------')
+
+with open('Moby-Dick.txt, 'r', encoding='utf-8') as livro:
+    linhas = livro.readlines()      
+     print(f'Foram impressas {len(linhas)} linhas.')
